74e3d12f8e21a9c380177e889c47974949202c4d
ChunSangwoo <aksyd23@gmail.com>
2026-05-07 17:30:23 +0900

    0507 수업

--- front1/src/components/CompLogin.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CompLogin() {
-  return (
-    <div style={{ width: "1000px", margin: "auto" }}>
-      <h1>Post :: 로그인 폼</h1>
-    </div>
-  );
-}
---
+++ portfolio-server/routes/footer.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/footer", (req, res, next) => {
+  const footer = {
+    description:
+      "Junior Software Engineer Judy's Portfolio - All right reserved",
+    list: [
+      { href: "#", icon: "github" },
+      { href: "#", icon: "linkedin" },
+    ],
+  };
+
+  res.json({ result: footer });
+});
+
+export default router;
